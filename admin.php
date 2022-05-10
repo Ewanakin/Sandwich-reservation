89cfa3973df798106cd80a49524073c2649618b7
ewan quioc <quioc.ewan@gmail.com>
2022-05-10 09:28:54 +0200

    ajout du fichier admin.php

---
+++ admin.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Page gestion des utilisateurs</title>
+    </head>
+    <body>
+        
+        <div class="navbar">
+            <a class="active" href="./index.php">Réservation <br> Sandwich</a>
+            <a style="float: right" href="#news">Se déconnecter</a>
+        </div>
+
+        <div id="table-scroll" class="table-scroll" style='max-width:500px;margin-top: 200px;'>
+            <div class="table-wrap">
+                <table class="main-table">
+                    <thead>
+                    <tr>
+                        <th class="fixed-side" scope="col">&nbsp;</th>
+                        <th scope="col">Liste des utilisateurs</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th class="fixed-side">ID 1</th>
+                        <td>Eleve 1</td>
+                        <td>Liste des sandwichs</td>
+                        <td>Modifier Supprimer</td>
+                    </tr>
+                    <tr>
+                        <th class="fixed-side">ID 2</th>
+                        <td>Eleve 2</td>
+                        <td>Liste des sandwichs</td>
+                        <td>Modifier Supprimer</td>
+                    </tr>
+                    <tr>
+                        <th class="fixed-side">ID 3</th>
+                        <td>Eleve 3</td>
+                        <td>Liste des sandwichs</td>
+                        <td>Modifier Supprimer</td>
+                    </tr>
+                    <tr>
+                        <th class="fixed-side">ID 4</th>
+                        <td>Eleve 4</td>
+                        <td>Liste des sandwichs</td>
+                        <td>Modifier Supprimer</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <footer>
+            <p>Footer Saint-Vincent</p>
+        </footer>
+        
+    </body>
+</html>
