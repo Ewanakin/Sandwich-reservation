0cf380dd129ce78698132b500826c663a7962bef
Allan Rondeau <sdrondeauallan@gmail.com>
2022-05-12 22:27:44 +0200

    Vérification de non envoie de commande en double

---
+++ Templates/footer.php
@@ -0,0 +1,12 @@
+<?php
+    echo'<head>
+            <link href="footer.css" rel="stylesheet">
+        <head>
+        <footer id="footerHome">
+            <div id="footerCont">
+                <img src="../Images/LogoLSV.png" alt="Logo lycée Saint-Vincent">
+                <h5> Site crée par les élèves Ewan Quioc, Enzo N ga et Allan Rondeau</h5>
+            </div>
+        </footer>';    
+?>
+
