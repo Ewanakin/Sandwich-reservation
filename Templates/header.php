35b8d5b6c886e48883fc7ba4e932de81d8f524ca
ewan quioc <quioc.ewan@gmail.com>
2022-05-12 19:13:10 +0200

    ajout du fichier header.css et ajout de l'affichage dynamique du header en fonction de l'utilisateur

--- Templates/header.php
+++ Templates/header.php
@@ -1,17 +1,59 @@
 <?php
-    require("Connexion/connexion.php");
+    require("../Connexion/connexion.php");
     $co = connexionBdd();
-    $reqUser = $co->prepare("SELECT * FROM utilisateur WHERE email_user = ? AND password_user = ?");
-    $reqUser->execute(array("administrateur@wanadoo.fr","password"));
-    $row =  $reqUser->fetch();
-    $existUser = 1;
-    if($existUser == 1)
-    {
-        echo $row["email_user"];
-        echo $row["password_user"];
-    }
-    if($existUser == 0)
-    {
-        
-    }
+    session_start();
+    $_SESSION["username"] = "test";
+    $_SESSION["role_user"] = "a";
 ?>
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Sandwicherie</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E">
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="header.css">
+    </head>
+    <body>
+
+        <header>
+                <?php
+                    //si l'utilisateur connecté est un administrateur
+                    if($_SESSION["username"] != "")
+                    {
+                        if($_SESSION["role_user"] == "a")
+                        {
+                            echo "<ul>";
+                                echo "<li><a href=''>Reservation Sandwich</a></li>";
+                                echo "<li><a href=''>Gestion des utilisateurs</a></li>";
+                                echo "<li><a href=''>Modification Accueil</a></li>";
+                                echo "<li><a href=''>Deconnexion</a></li>";
+                            echo "</ul>";
+                        }
+                        else
+                        {   
+                            echo "<ul>";
+                                echo "<li><a href=''>Reservation Sandwich</a></li>";
+                                echo "<li><a href=''>passer une commande</a></li>";
+                                echo "<li><a href=''>historique des commandes</a></li>";
+                                echo "<li><a href=''>Deconnexion</a></li>";
+                            echo "</ul>";
+                        }
+                    }
+                    //si aucun utilisateur n'est connecté
+                    if($_SESSION["username"] == "")
+                    {
+                        echo "<ul>";
+                            echo "<li><a href=''>Reservation Sandwich</a></li>";
+                            echo "<li><a href=''>S'inscrire</a></li>";
+                            echo "<li><a href=''>Se connecter</a></li>";
+                        echo "</ul>";
+                    }
+                ?>
+            </ul>
+        </header>
+
+
+    </body>
+</html>
