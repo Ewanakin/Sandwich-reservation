0d76d79af5942c8ec6e3fdd35d471a7d20dcd50d
AllanRondeau <sdrondeauallan@gmail.com>
2022-05-15 20:35:30 +0200

    changement de la gestion du style du footer

--- Templates/footer.php
+++ Templates/footer.php
@@ -1,11 +1,9 @@
 <?php
-    echo'<head>
-            <link href="footer.css" rel="stylesheet">
-        <head>
-        <footer id="footerHome">
-            <div id="footerCont">
-                <img src="../Images/LogoLSV.png" alt="Logo lycée Saint-Vincent">
-                <h5> Site crée par les élèves Ewan Quioc, Enzo N ga et Allan Rondeau</h5>
+    echo'
+        <footer id="footerHome" style="width: 100vw;  background-color: #1e2d4c; position: absolute; bottom: 0;">
+            <div id="footerCont" style="display: flex; flex-direction: column; align-items: center;">
+                <img src="../Images/LogoLSV.png" alt="Logo lycée Saint-Vincent" style="height: 6vh; margin-top: 3vh;">
+                <h5 style = "font-size: 20vw; color: white; letter-spacing: 1px; font-weight: lighter;"> Site crée par les élèves Ewan Quioc, Enzo N ga et Allan Rondeau</h5>
             </div>
         </footer>';    
 ?>
